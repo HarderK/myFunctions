5bae92c00433907129e64cfe1df693e09423dea7
Harder <592917974@qq.com>
2017-05-13 11:30:36 +0800

    add curry

---
+++ Curry.js
@@ -0,0 +1,45 @@
+/**
+ * 函数柯里化
+ * @param  {Function} fn 需要柯里化的函数
+ * @return {Function}      
+ */
+var curry = function(fn) {
+	var args = [];		// 存放fn需要的参数
+
+	return function func() {
+		if(arguments.length === 0) {		// 如果不传入参数
+			return fn.apply(this, args);	// 执行fn
+		} else {		// 传入参数则不执行fn，将参数加入args中
+			Array.prototype.push.apply(args, arguments);
+			return func;	//链式调用
+		}
+	};
+};
+
+// 上面的柯里化函数的逻辑是：当给柯里化的函数不传入参数的时候就执行函数fn
+// 参考lodash里面的curry函数, fn执行的时机是接受到了所需要个数参数，而不是不传入参数，不传入参数实际上是多调用了一次
+
+/**
+ * 函数柯里化
+ * @param  {Function} fn    需要柯里化的函数
+ * @param  {Number}   arity fn的参数个数不确定的时候用来制定其需要的参数个数，默认为fn.length
+ * @return {Function}         curry
+ */
+var curry = function(fn, arity) {
+	arity = arity || fn.length;		// 柯里化函数需要的参数个数
+	var args = [];		// 保存参数
+
+	return function func() {
+			Array.prototype.push.apply(args, arguments);		// 将参数加入args中
+
+			if(args.length < arity) {		// 参数个数不够
+				return func;		// 链式调用
+			} else if(args.length == arity) {	// 参数个数与所需相等
+				return fn.apply(this, args);	// 执行fn
+			} else {		// 参数个数超过所需个数
+				return fn.apply(this, args.slice(0, arity));	// 截取args为所需的个数，并执行fn
+			}
+	};
+};
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 一些实用函数
 1. [bind()](https://github.com/HarderK/myFunctions/blob/master/Bind.js)。也就是ES5中的bind()方法
+2. [curry()函数柯里化](https://github.com/HarderK/myFunctions/blob/master/Curry.js)
